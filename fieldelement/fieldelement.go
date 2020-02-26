7e372f4a3f36ccf9b6d4a17eb9fa2aaba129b1b4
Keith Asilom <47@konsum.ph>
2020-02-26 12:23:23 +0800

    create struct
    create fieled element struct

---
+++ fieldelement/fieldelement.go
@@ -0,0 +1,10 @@
+package fieldelement
+
+type num float64
+type prime float64
+
+// FieldElement is the struct representation of an element
+type FieldElement struct {
+	num   float64
+	prime float64
+}
